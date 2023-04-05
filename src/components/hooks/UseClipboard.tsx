15c1c38979a4033f10541069e6acd4f2477ac9be
Cesar <cesarvillalobosolmos.01@gmail.com>
2023-04-05 07:27:40 -0600

    :sparkles: Add UseClipboard component

---
+++ src/components/hooks/UseClipboard.tsx
@@ -0,0 +1,40 @@
+import { useClipboard, Center, VStack, HStack, Input, Button } from 'native-base';
+import { useState } from 'react';
+
+export const UseClipboard = (): JSX.Element => {
+  const [ copyText, setCopyText ] = useState<string>("Hello");
+  const [ pasteText, setPasteText ] = useState<string>("");
+  
+  const {
+    value,
+    onCopy
+  } = useClipboard();
+
+  return <Center>
+    <VStack space={2} w="100%" maxW="300">
+      <HStack space={3}>
+        <Input 
+          placeholder="Copy From" 
+          onChangeText={v => setCopyText(v)} 
+          value={copyText} 
+        />
+        
+        <Button onPress={() => onCopy(copyText)}>
+          Copy
+        </Button>
+      </HStack>
+
+      <HStack space={3}>
+        <Input 
+          placeholder="Paste Here" 
+          onChangeText={v => setPasteText(v)} 
+          value={pasteText} 
+        />
+        
+        <Button onPress={() => setPasteText(value)}>
+          Paste
+        </Button>
+      </HStack>
+    </VStack>
+  </Center>;
+};
